d76190615cc5b2f4fc0c6b44fd31c5104ecb3861
piramidonwitch <103138025+piramidonwitch@users.noreply.github.com>
2022-07-09 23:11:47 +0300

    Update index.js
    add string

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function publish() {
     let p = document.getElementById("out");
     let a = Number(document.getElementById("num1").value);
     let b = Number(document.getElementById("num2").value);
-    p.innerHTML = a + b;
+    p.innerHTML = a + b + " блядских лебядзей!";
 }
