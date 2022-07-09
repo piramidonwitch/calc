94bb5ac526157df308afb2310e785745a64e2747
piramidonwitch <103138025+piramidonwitch@users.noreply.github.com>
2022-07-09 22:46:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function publish() {
+    let p = document.getElementById("out");
+    let a = Number(document.getElementById("num1").value);
+    let b = Number(document.getElementById("num2").value);
+    p.innerHTML = a + b;
+}
